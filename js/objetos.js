ca1ce5490c6738b493f08f9b5660cf6c44f78cff
Vittorio <viegoxaspar@gmail.com>
2025-05-19 09:43:37 -0300

    07 - adicionamos as imagens na pasta images - criação do objeto (shows) - criação do arquivo montarFront.js - importação do objeto shows

---
+++ js/objetos.js
@@ -0,0 +1,243 @@
+export const shows = [{
+        artista: 'Red Hot Chili Peppers',
+        local: 'Belo Horizonte',
+        data: '2025-06-12',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image0.jpg'
+    },
+    {
+        artista: 'Arctic Monkeys',
+        local: 'São Paulo',
+        data: '2025-07-25',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image1.jpg'
+    },
+    {
+        artista: 'Foo Fighters',
+        local: 'Recife',
+        data: '2025-08-05',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image2.jpg'
+    },
+    {
+        artista: 'The Killers',
+        local: 'Curitiba',
+        data: '2025-07-01',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image3.jpg'
+    },
+    {
+        artista: 'Muse',
+        local: 'Natal',
+        data: '2025-12-18',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image4.jpg'
+    },
+    {
+        artista: 'Radiohead',
+        local: 'Manaus',
+        data: '2025-08-23',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image5.jpg'
+    },
+    {
+        artista: 'Green Day',
+        local: 'Salvador',
+        data: '2025-09-07',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image6.jpg'
+    },
+    {
+        artista: 'Metallica',
+        local: 'Vitória',
+        data: '2025-06-28',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image7.jpg'
+    },
+    {
+        artista: 'Nirvana',
+        local: 'Goiânia',
+        data: '2025-11-11',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image8.jpg'
+    },
+    {
+        artista: 'Pearl Jam',
+        local: 'Fortaleza',
+        data: '2025-10-02',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image9.jpg'
+    },
+    {
+        artista: 'The Strokes',
+        local: 'Porto Alegre',
+        data: '2025-07-14',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image0.jpg'
+    },
+    {
+        artista: 'U2',
+        local: 'Brasília',
+        data: '2025-09-27',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image1.jpg'
+    },
+    {
+        artista: 'Linkin Park',
+        local: 'Belém',
+        data: '2025-06-20',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image2.jpg'
+    },
+    {
+        artista: 'Coldplay',
+        local: 'Rio de Janeiro',
+        data: '2025-12-01',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image3.jpg'
+    },
+    {
+        artista: 'Queens of the Stone Age',
+        local: 'Florianópolis',
+        data: '2025-08-16',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image4.jpg'
+    },
+    {
+        artista: 'Muse',
+        local: 'Belo Horizonte',
+        data: '2025-11-08',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image5.jpg'
+    },
+    {
+        artista: 'Foo Fighters',
+        local: 'São Paulo',
+        data: '2025-10-21',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image6.jpg'
+    },
+    {
+        artista: 'Green Day',
+        local: 'Recife',
+        data: '2025-06-06',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image7.jpg'
+    },
+    {
+        artista: 'Red Hot Chili Peppers',
+        local: 'Natal',
+        data: '2025-09-02',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image8.jpg'
+    },
+    {
+        artista: 'Arctic Monkeys',
+        local: 'Curitiba',
+        data: '2025-07-10',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image9.jpg'
+    },
+    {
+        artista: 'The Killers',
+        local: 'Fortaleza',
+        data: '2025-12-12',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image1.jpg'
+    },
+    {
+        artista: 'Pearl Jam',
+        local: 'Goiânia',
+        data: '2025-07-31',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image2.jpg'
+    },
+    {
+        artista: 'The Strokes',
+        local: 'Florianópolis',
+        data: '2025-09-15',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image3.jpg'
+    },
+    {
+        artista: 'Radiohead',
+        local: 'Porto Alegre',
+        data: '2025-11-26',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image4.jpg'
+    },
+    {
+        artista: 'Nirvana',
+        local: 'Salvador',
+        data: '2025-10-18',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image5.jpg'
+    },
+    {
+        artista: 'Metallica',
+        local: 'Vitória',
+        data: '2025-08-30',
+        horario: '21:00',
+        tipo_ingresso: 'meia entrada',
+        banner: '../images/image6.jpg'
+    },
+    {
+        artista: 'Coldplay',
+        local: 'Rio de Janeiro',
+        data: '2025-07-19',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image7.jpg'
+    },
+    {
+        artista: 'Linkin Park',
+        local: 'Brasília',
+        data: '2025-06-15',
+        horario: '21:00',
+        tipo_ingresso: 'premium',
+        banner: '../images/image8.jpg'
+    },
+    {
+        artista: 'U2',
+        local: 'Belém',
+        data: '2025-11-03',
+        horario: '21:00',
+        tipo_ingresso: 'pista',
+        banner: '../images/image9.jpg'
+    },
+    {
+        artista: 'Queens of the Stone Age',
+        local: 'Manaus',
+        data: '2025-12-27',
+        horario: '21:00',
+        tipo_ingresso: 'vip',
+        banner: '../images/image0.jpg'
+    }
+];
+  
+
